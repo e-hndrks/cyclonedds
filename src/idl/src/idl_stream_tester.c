28be030da80c8855af873e83b595021ed30a47c8
Martijn Reicher <martijn.reicher@adlinktech.com>
2020-07-30 12:58:09 +0200

    Made code more modular through separating repeatedly used functionality into their own functions. Added support for template structures: strings, sequences, fixed points.
    Signed-off-by: Martijn Reicher <martijn.reicher@adlinktech.com>

--- src/idl/src/idl_stream_tester.c
+++ src/idl/src/idl_stream_tester.c
@@ -7,31 +7,47 @@
 
 int main(int argc, char **argv)
 {
+  if (argc < 2)
+  {
+    fputs("a file name needs to be supplied when starting the converter", stderr);
+    return -1;
+  }
   printf("testing\n");
 
   char* source = NULL;
   FILE* fp = fopen(argv[1], "r");
 
   if (NULL == fp)
-    return -1;
+  {
+    fprintf(stderr, "could not open the file at location %s", argv[1]);
+    return -2;
+  }
 
-  if (fseek(fp, 0L, SEEK_END) == 0) {
+  if (fseek(fp, 0L, SEEK_END) == 0)
+  {
     /* Get the size of the file. */
     long bufsize = ftell(fp);
-    if (bufsize == -1) { /* Error */ }
+    if (bufsize == -1)
+    {
+      fputs("could not find the end of the file", stderr);
+      return -3;
+    }
 
     /* Allocate our buffer to that size. */
-    source = malloc(sizeof(char) * (bufsize + 1));
+    source = malloc(sizeof(char) * ((size_t)bufsize + 1));
 
     /* Go back to the start of the file. */
     if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
 
     /* Read the entire file into memory. */
-    size_t newLen = fread(source, sizeof(char), bufsize, fp);
-    if (ferror(fp) != 0) {
+    size_t newLen = fread(source, sizeof(char), (size_t)bufsize, fp);
+    if (ferror(fp) != 0)
+    {
       fputs("Error reading file", stderr);
+      return -4;
     }
-    else {
+    else
+    {
       source[newLen++] = '\0'; /* Just to be safe. */
     }
   }
